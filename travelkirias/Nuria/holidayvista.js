0f15ac356cf7af232ab8d2406091d4ef0608299e
Nuriavalle <valle.nuria.info1@gmail.com>
2024-04-14 00:03:19 -0600

    gallery

--- travelkirias/Nuria/holidayvista.js
+++ travelkirias/Nuria/holidayvista.js
@@ -1,30 +1,30 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const carouselItems = document.querySelectorAll('.carousel-item');
-    let currentIndex = 0;
+// document.addEventListener('DOMContentLoaded', function () {
+//     const carouselItems = document.querySelectorAll('.carousel-item');
+//     let currentIndex = 0;
 
-    function showItem(index) {
-      carouselItems.forEach(item => {
-        item.classList.remove('opacity-100');
-        item.classList.add('opacity-0', 'hidden');
-      });
+//     function showItem(index) {
+//       carouselItems.forEach(item => {
+//         item.classList.remove('opacity-100');
+//         item.classList.add('opacity-0', 'hidden');
+//       });
 
-      carouselItems[index].classList.remove('opacity-0', 'hidden');
-      carouselItems[index].classList.add('opacity-100');
-    }
+//       carouselItems[index].classList.remove('opacity-0', 'hidden');
+//       carouselItems[index].classList.add('opacity-100');
+//     }
 
-    function nextItem() {
-      currentIndex = (currentIndex + 1) % carouselItems.length;
-      showItem(currentIndex);
-    }
+//     function nextItem() {
+//       currentIndex = (currentIndex + 1) % carouselItems.length;
+//       showItem(currentIndex);
+//     }
 
-    function prevItem() {
-      currentIndex = (currentIndex - 1 + carouselItems.length) % carouselItems.length;
-      showItem(currentIndex);
-    }
+//     function prevItem() {
+//       currentIndex = (currentIndex - 1 + carouselItems.length) % carouselItems.length;
+//       showItem(currentIndex);
+//     }
 
-    document.getElementById('prevBtn').addEventListener('click', prevItem);
-    document.getElementById('nextBtn').addEventListener('click', nextItem);
+//     document.getElementById('prevBtn').addEventListener('click', prevItem);
+//     document.getElementById('nextBtn').addEventListener('click', nextItem);
 
-    // Mostrar el primer elemento al cargar la página
-    showItem(currentIndex);
-  });
+//     // Mostrar el primer elemento al cargar la página
+//     showItem(currentIndex);
+//   });
